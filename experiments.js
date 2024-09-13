95ee1edb3dd22a5586bf5cc1b37768069ab8d003
ola pacia <olusiaop05@gmail.com>
2024-09-14 00:12:40 +0200

    substring builtin function

--- experiments.js
+++ experiments.js
@@ -1,6 +1,13 @@
-const timesToRepeat = 10;
-const character ='D';
 
-for (i=0; i<timesToRepeat0; i++ ) {
-    console.log(character);
+
+const yourCity = "Rudy";
+const yourCountry = "Poland";
+function logOutYourHome(yourCity, yourCountry) {
+    console.log(`This is your city: ${yourCity} and this is ${yourCountry}!`)
 }
+
+logOutYourHome(yourCity, yourCountry);
+
+let string = "hi my name is Ola"; 
+string = string.substring(3);
+console.log(string)
