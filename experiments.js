f5e2a0208d5b300a39108250aa091e14489c9413
ola pacia <olusiaop05@gmail.com>
2024-09-14 15:55:25 +0200

    object in JavaScript

--- experiments.js
+++ experiments.js
@@ -11,3 +11,14 @@ logOutYourHome(yourCity, yourCountry);
 let string = "hi my name is Ola"; 
 string = string.substring(3);
 console.log(string)
+
+const person = {
+    name: "Ola",
+    city: "Gliwice",
+    voivodship: "Silesia",
+    favoriteFood: "Pizza",
+    wantsPizzaRightNow: true,
+    numberOfPizzaPiecesWanted: 100,
+}
+
+console.log(person.name)
